dc8f2b348385310a427c147ef36304dafce8004c
Farhat Sharif <farhatsharif.it@gmail.com>
2020-11-30 22:54:40 +0500

    Initial commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  newPost = 'NO content';
+  enteredValue = '';
+
+  onAddPost(inputE: HTMLTextAreaElement) {
+    this.newPost = inputE.value;
+  }
+}
