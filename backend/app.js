05a3eaf8f8c73731fc4ef7cc944e51185fb61bfd
Farhat Sharif <farhatsharif.it@gmail.com>
2020-12-02 16:33:03 +0500

    Fetch posts (using dummy data)

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first req');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("my response back");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'hiah8324ye',
+      Title: 'My first post',
+      Content: 'using Express framework for building the backend'
+    },
+    {
+      id: 'hiah8324ye',
+      Title: 'Another post',
+      Content: 'Enhancing the backend'
+    }
+  ];
+  return res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
