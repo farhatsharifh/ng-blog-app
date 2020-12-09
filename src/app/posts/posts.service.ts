ada3106c7f243152478421780553659ec62b7d2c
Farhat Sharif <farhatsharif.it@gmail.com>
2020-12-10 01:46:21 +0500

    Upload file from Client side

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -41,12 +41,21 @@ export class PostsService {
     return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3002/api/posts/" + id);
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = { id: null, title: title, content: content };
-    this.http.post<{message: string, postId: string}>("http://localhost:3002/api/posts", post)
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title)
+    this.http.post<{message: string, postId: string}>(
+      "http://localhost:3002/api/posts",
+      postData
+      )
       .subscribe((resData) => {
-        const id = resData.postId;
-        post.id = id;
+        const post: Post = {
+          "id": resData.postId,
+          "title": title,
+          "content": content
+        };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
