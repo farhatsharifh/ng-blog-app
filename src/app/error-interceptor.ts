722420328247e1ac0175cc4f7bf6ee4dd82a4338
Farhat Sharif <farhatsharif.it@gmail.com>
2020-12-21 03:48:36 +0500

    Create ErrorComponent to display error dialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,15 +1,25 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 
+import { ErrorComponent } from "./error/error.component";
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(
+    private dialog: MatDialog
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
+        let errorMessage = "An unknown error occurred";
+        if(error.error.error.message) {
+          errorMessage = error.error.error.message;
+        }
+        this.dialog.open(ErrorComponent, {data: { message: errorMessage }});
         return throwError(error);
       })
     );
