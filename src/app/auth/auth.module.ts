02d49bee23b8cb33c0bd0557ed9fe336561fa502
Farhat Sharif <farhatsharif.it@gmail.com>
2020-12-22 03:18:59 +0500

    Add lazy loading

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from "@angular/forms";
 import { AngularMaterialModule } from "../angular-material.module";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
+import { AuthRoutingModule } from "./auth-routing.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SignupComponent } from "./signup/signup.component";
   imports: [
     CommonModule,
     AngularMaterialModule,
-    FormsModule
+    FormsModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule {}
